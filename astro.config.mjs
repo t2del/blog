40aa608e4867b431680b9b2ebc0d3ee7dd2d0e47
Dennis Macapagal <macapagal.dennis@gmail.com>
2024-09-27 16:21:24 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.t2del.com/',
+    site: 'http://t2del.github.io/',
     base: '/blog/',
     // output: 'server',
     // adapter: nodejs(),
